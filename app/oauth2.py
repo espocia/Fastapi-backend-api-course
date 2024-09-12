0d0002baef196b1b3df4dd57ff3c0eec067e13ae
JJ <jespocia@agsx.net>
2024-09-13 03:20:12 +0800

    add: jwt exception

--- app/oauth2.py
+++ app/oauth2.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta, timezone
 import jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jwt import InvalidTokenError
+from jwt.exceptions import InvalidTokenError
 from sqlalchemy.orm import Session
 from app.config import Settings
 
